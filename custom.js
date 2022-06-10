960a2ee3e02b29f323c4fce1ae4be676e0db4098
Rakesh <>
2022-06-10 10:59:56 +0530

    sda

--- custom.js
+++ custom.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function() {
-
+    /**JavaScript Arrays
+     * -Arrays are linear data structure
+     * -object type data structure
+     * -key value pair and its numeric indexed array
+     */
+    const arr = [];
+    console.log(arr);
 });
