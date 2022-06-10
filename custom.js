7625c722d5606b194f4d677a347227e7bb6e922c
Rakesh <>
2022-06-10 11:25:05 +0530

    arr strt

--- custom.js
+++ custom.js
@@ -4,6 +4,22 @@ jQuery(document).ready(function() {
      * -object type data structure
      * -key value pair and its numeric indexed array
      */
-    const arr = [];
-    console.log(arr);
+    const arr = ['abc', 'asd', 'awert', 'hgdre', 'asd', 'rtgtr', 'tgt'];
+    //push
+    //unshift
+    //pop
+    //shift
+    //indexof 
+    console.log(arr.indexOf('asd', 2));
+    //lastindexof
+    console.log(arr.lastIndexOf('asd', 2));
+    //includes  -work on only primitive data types
+    console.log(arr.includes('asd', 6));
+
+    const arrObj = [{ name: 'abc', age: 45 }, { name: 'asd', age: 85 }, { name: 'awert', age: 75 }, { name: 'hgdre', age: 95 }, { name: 'asd', age: 55 }, { name: 'rtgtr', age: 65 }, { name: 'tgt', age: 15 }];
+
+    //find
+    console.log(arrObj.find((elm) => { return elm.age > 85 }));
+    // console.log(typeof(arr));
+    console.log(arrObj);
 });
